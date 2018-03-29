b23865ba0549af2ebf68b39dfd02038c87f6903e
invot <invot15@hotmail.com>
2018-03-29 09:16:45 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Bootstrap 4 Razor Pages
+
+Using new Dotnet Core 2 Razor pages with Bootstrap 4
+
+![image](https://cloud.githubusercontent.com/assets/14539/26036748/fa4dddfc-38e3-11e7-9246-2d2a8077b3b2.png)
+
+## Features
+
+- NPM
+- Gulpfile
+- SASS
+- Bootstrap 4 starter template
+- FontAwesome 5 integration
+
+## Usage
+
+Install dependencies for `dotnet` and `NPM`:
+
+```bash
+dotnet restore
+
+npm install
+```
+
+after using `dotnet restore`.
+
+To run website:
+
+```bash
+dotnet run
+```
+
+## Author
+
+@invot
---
+++ Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+
+namespace Bootstrap4
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            // server vendor fiels from custom virtual directory to match /lib/{vendor}
+            // original paths in Razor markup from tempaltes
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), @"node_modules")),
+                RequestPath = new PathString("/lib")
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
